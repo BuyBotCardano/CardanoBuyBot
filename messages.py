5ddbb79082a7e833a6712120953a6030a4d5dc20
CardanoBuyBot <info.1one@yandex.ru>
2025-03-03 16:51:41 +0000

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,25 @@
+START_MSG = """
+To start using the Cardano Buy Bot, you need to click the "Add me to your group" button. 
+Then select the group where you want to add cardanobuy_bot. 
+Make sure you add the bot as an administrator. 
+The bot will send a message to the group with a button that will take you back to your private chat with the bot.
+"""
+
+IN_GROUP_MSG = """
+Thank you for adding me to the group! Make sure you add the bot as an administrator.
+
+Click the button below to go to the purchase settings.
+"""
+
+BACK_TO_BOT_MSG = """
+↪️ Paste the contract address of your token for the chat <b>"{}"</b>
+"""
+
+INVALID_CONTRACT_MSG = """
+❌ <b>This doesn't look like a token contract! Try again.</b>
+"""
+
+TOKEN_ADDED_MSG = """
+✅ ${} <b>has been successfully added!</b>
+You can either start the bot immediately with the default settings by clicking "Toggle", or choose "Token settings" to configure it according to your needs first.
+"""
