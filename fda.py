954326912caac4625988c896df34295ec76f9eba
CardanoBuyBot <info.1one@yandex.ru>
2025-03-03 16:47:50 +0000

    Create fda.py

---
+++ fda.py
@@ -0,0 +1,25 @@
+import aiohttp
+import asyncio
+
+
+async def fetch_data():
+    params = {
+        "timeframe": "all",
+        "sortBy": "time",
+        "order": "desc",
+        "perPage": 10,
+        "from": 0,
+        "page": 1,
+        "unit": "a2944573e99d2ed3055b808eaa264f0bf119e01fc6b18863067c63e44d454c44"
+    }
+
+    headers = {
+        "x-api-key": "YOUR_API_KEY"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://openapi.taptools.io/api/v1/token/trades", headers=headers, params=params) as response:
+            print(await response.text())
+
+
+asyncio.run(fetch_data())
